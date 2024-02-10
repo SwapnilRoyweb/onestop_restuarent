2569ea8fd739388151c0a6511ea35f47b39114fd
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-02-10 15:39:46 +0600

    some new design applied to restuarent card

--- src/Components/RestuarentCard/RestuarentCard.jsx
+++ src/Components/RestuarentCard/RestuarentCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaRegHandPointRight } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
 
 const RestuarentCard = ({ restuarent }) => {
 
@@ -9,6 +10,7 @@ const RestuarentCard = ({ restuarent }) => {
 
     return (
         <div className='w-80 lg:w-96 h-64 lg:h-80 rounded-2xl relative'>
+            <p className='absolute top-3 right-3 w-fit px-3 bg-yellow-500 text-center text-white rounded-lg flex flex-row items-center gap-1'>{Ratings}<FaStar/></p>
             <div className='absolute inset-0 flex flex-col items-start p-8 justify-end transition-opacity duration-300 bg-black bg-opacity-70 opacity-0  hover:opacity-90 rounded-2xl text-white'>
                 <div className='flex flex-row justify-between items-end w-full'>
                     <div>
@@ -17,7 +19,7 @@ const RestuarentCard = ({ restuarent }) => {
                         <p className='font-semibold'>Category: {Type}</p>
                     </div>
                     <button className="btn btn-circle btn-outline hover:bg-slate-900" title='Details'>
-                        <FaRegHandPointRight className='text-2xl text-white'/>
+                        <FaRegHandPointRight className='text-2xl text-white' />
                     </button>
                 </div>
             </div>
