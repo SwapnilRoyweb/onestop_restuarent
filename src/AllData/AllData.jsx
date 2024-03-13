97c773b333a1b65efa2f10455e2e79f640d8449b
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-03-13 22:32:06 +0600

    filter button added and also some changes applied

--- src/AllData/AllData.jsx
+++ src/AllData/AllData.jsx
@@ -15,7 +15,7 @@ const AllData = () => {
 
     return (
         <div className='w-full flex justify-center items-center'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-5'>
                 {
                     restuarents.map(restuarent => <RestuarentCard key={restuarent._id} restuarent={restuarent}></RestuarentCard>)
                 }
--- src/Shared/Nav/Navbar.jsx
+++ src/Shared/Nav/Navbar.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdYoutubeSearchedFor } from "react-icons/md";
+import { TbFilterBolt } from "react-icons/tb";
 
 const Navbar = () => {
     return (
         <div className='w-full flex flex-row items-center justify-around'>
-            <h1 className='text-3xl lg:text-5xl uppercase font-extrabold'>Onestop</h1>
+            <h1 className='text-3xl lg:text-5xl uppercase font-extrabold mt-3'>Onestop</h1>
 
             {/* Search bar */}
-            <div className='flex flex-col justify-center items-center mt-5 lg:mt-8'>
+            <div className='flex flex-row gap-3 justify-center items-center mt-5'>
                 <label htmlFor="search"></label>
                 <div className='flex flex-row justify-center items-center gap-1'>
                     <input type="text" name="" id="" className='w-11/12 md:w-96 p-3 rounded-full bg-gradient-to-l from-[#FFDEE9] to-[#B5FFFC] text-xl font-semibold' />
                     <button className='bg-gradient-to-l from-[#0093E9] to-[#80D0C7] p-2 rounded-full -ml-12 transition ease-in-out delay-150 hover:scale-125 duration-300 '><MdYoutubeSearchedFor className='text-xl font-bold' /></button>
                 </div>
+                <div className="dropdown dropdown-bottom lg:dropdown-right">
+                    <div tabIndex={0} role="button" className="m-1 btn h-fit btn-accent btn-xs sm:btn-sm md:btn-md"><TbFilterBolt className='text-md md:text-xl' /><span className='font-bold'>Filter</span></div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-3 shadow bg-white rounded-box w-40 text-black">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </div>
+                {/* <button className="btn h-fit btn-accent btn-xs sm:btn-sm md:btn-md"></button> */}
             </div>
+
 
             <div>
                 <p>Profile</p>
