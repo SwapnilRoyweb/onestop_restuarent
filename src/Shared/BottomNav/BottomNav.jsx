af37afd01f9a5df78f42cb6071cbccb9929e8de1
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-03-21 01:13:34 +0600

    bottom navbar updated and new design applied

--- src/Shared/BottomNav/BottomNav.jsx
+++ src/Shared/BottomNav/BottomNav.jsx
@@ -4,13 +4,13 @@ import { FaSearchLocation, FaHome, FaUserCog  } from "react-icons/fa";
 const BottomNav = () => {
     return (
         <div className="flex sm:hidden btm-nav btm-nav-xs rounded-full bottom-5 text-white">
-            <button>
+            <button className='hover:active bg-pink-200 text-pink-600 hover:border-pink-600 hover:border-2 hover:bg-pink-200 rounded-s-full'>
                 <FaHome></FaHome>
             </button>
-            <button className="active">
+            <button className="hover:active bg-blue-200 text-blue-600 hover:border-blue-600 hover:border-2 hover:bg-blue-200">
                 <FaSearchLocation></FaSearchLocation>
             </button>
-            <button>
+            <button className='hover:active bg-teal-200 text-teal-600 hover:border-teal-600 hover:border-2 hover:bg-teal-200 rounded-e-full'>
                 <FaUserCog></FaUserCog>
             </button>
         </div>
